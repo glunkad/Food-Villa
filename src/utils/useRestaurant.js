9b8c9cf3c89063c1eb0bbc659724a986bc7c006f
glunkad <glunkad26@gmail.com>
2024-09-01 20:04:39 +0530

    feat: added changes

---
+++ src/utils/useRestaurant.js
@@ -0,0 +1,25 @@
+import {useEffect, useState} from "react";
+
+const useRestaurant = (resId) => {
+    const [restaurant, setRestaurant] = useState(null);
+    const [menu, setMenu] = useState([])
+
+    useEffect(() => {
+        getRestaurantInfo()
+    },[])
+
+    async function getRestaurantInfo() {
+        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.61610&lng=73.72860&restaurantId="+resId+"&catalog_qa=undefined&submitAction=ENTER")
+        const json = await data.json();
+        const restaurantInfoCard = json.data.cards.find(card => card.card.card["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.Restaurant")
+        setRestaurant(restaurantInfoCard?.card?.card?.info)
+
+        const groupedCard = json.data.cards.find(card => card.groupedCard);
+        const menuCard = groupedCard?.groupedCard?.cardGroupMap?.REGULAR?.cards.find(card => card.card.card["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.MenuCarousel")
+        setMenu(menuCard?.card?.card?.carousel)
+    }
+
+    return [restaurant,menu];
+}
+
+export default useRestaurant;
